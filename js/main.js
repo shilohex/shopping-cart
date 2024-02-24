a2b5aa3f5973cc403045933fadad78c7a22f6140
Excel oladipupo <shiloholadipupo@gamil.com>
2024-02-24 14:27:40 +0100

    javascript

--- js/main.js
+++ js/main.js
@@ -1,5 +1,22 @@
-function toggleHeart() {
-    var heartButton = document.getElementById('heartButton');
-    heartButton.classList.toggle('clicked');
-  }
+let items = document.querySelectorAll('.case11')
   
+items.forEach(item =>{
+  const price= parseFloat(item.querySelector('.price').innerHTML.slice(1))
+  const inc = item.querySelector('.inc')
+  const dec = item.querySelector('.dec')
+  const total = item.querySelector('.total')
+  const qty =item.querySelector('.qty')
+  let quantity = parseFloat(item.querySelector('.qty').innerHTML)
+
+  inc.addEventListener("click",()=>{
+   quantity++
+   console.log(quantity);
+   console.log(price);
+   quantity.innerHTML = quantity
+    total.innerHTML=`$${price*quantity}`
+  })
+})
+
+dec.addEventListener("click",()=>{
+  total.innerHTML = `$${price*quantity}`
+})
