398676aab4f22fc4ecd8066e5752bc1490f8fb2e
David <okekedavid1333@gmail.com>
2024-02-24 16:02:08 +0100

    dec added and bug fix

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
-let items = document.querySelectorAll('.case12')
+let items = document.querySelectorAll('.case1')
   
 items.forEach(case12 =>{
   const price= parseFloat(case12.querySelector('.price').innerHTML)
   const inc = case12.querySelector('.inc')
   const dec = case12.querySelector('.dec')
   const total = case12.querySelector('.total')
-  const qty =case12.querySelector('.qty')
-  let quantity = parseInt(case12.querySelector('.qty').innerHTML)
+  let qty =case12.querySelector('.qty')
+  let quantity = parseInt(qty.textContent)
 
   inc.addEventListener("click",()=>{
    quantity++
@@ -16,7 +16,11 @@ items.forEach(case12 =>{
     total.innerHTML=`$${price*quantity}`
   })
   dec.addEventListener("click",()=>{
-  total.innerHTML = `$${price*quantity}`
+    quantity--
+    console.log(quantity);
+    console.log(price);
+    qty.innerHTML = quantity
+     total.innerHTML=`$${price*quantity}`
 })
 
 
